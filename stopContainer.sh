9a20d17dffe4786dcb0d22f44d075e6d8f07a297
Scott Aaron <dr_saaron@yahoo.com>
2021-11-09 12:06:51 -0600

    version update

--- stopContainer.sh
+++ stopContainer.sh
@@ -4,6 +4,9 @@
 # a not-root user docker stop can't kill those processes.  So explicitly kill
 # the mysqld process
 
-mysqldPID=$(ps -ef | grep [m]ysqld | awk '{ print $2 }')
-echo $mysqldPID
-[ "$mysqldPID" = "" ] || kill $mysqldPID
+#mysqldPID=$(ps -ef | grep [m]ysqld | awk '{ print $2 }')
+#echo $mysqldPID
+#[ "$mysqldPID" = "" ] || kill $mysqldPID
+
+# apparently now we can just doa simple stop.
+docker stop mysql1
